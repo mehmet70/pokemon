d4d78ebf43ee5d30c50d260c7d6aaee491be4de2
Mehmet Balci <50658677+mehmet70@users.noreply.github.com>
2024-07-03 11:57:19 +0200

    Format

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,49 +1,106 @@
 <script setup>
-import { useDark, useToggle } from "@vueuse/core"
+import { useDark, useToggle } from "@vueuse/core";
 
-    const isDark = useDark();
-    const toggleDark = useToggle(isDark)
-
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
 </script>
 
 <template>
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Pok%C3%A9_Ball_icon.svg" class="h-8" alt="Flowbite Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Pokemon</span>
-    </a>
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+  <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div
+      class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+    >
+      <a class="flex items-center space-x-3 rtl:space-x-reverse">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/5/53/Pok%C3%A9_Ball_icon.svg"
+          class="h-8"
+          alt="Flowbite Logo"
+        />
+        <span
+          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+          >Pokemon</span
+        >
+      </a>
+      <button
+        data-collapse-toggle="navbar-default"
+        type="button"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-default"
+        aria-expanded="false"
+      >
         <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        <svg
+          class="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 17 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M1 1h15M1 7h15M1 13h15"
+          />
         </svg>
-    </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-            <nuxt-link to="/" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" exact-active-class="md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">Home</nuxt-link>
-        </li>
-        <li>
-            <nuxt-link to="/about" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" exact-active-class="md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">About</nuxt-link>
-        </li>
-        <li>
-            <nuxt-link to="/Services" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" exact-active-class="md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">Services</nuxt-link>
-        </li>
-        <li>
-            <nuxt-link to="/Pricing" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" exact-active-class="md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">Pricing</nuxt-link>
-        </li>
-        <li>
-            <nuxt-link to="/Contact" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" exact-active-class="md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">Contact</nuxt-link>
-        </li>
-        <li>
-          <button id="theme-toggle" @click="toggleDark()" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
-                {{ isDark ? "Dark" : "light" }}
-          </button>
-        </li>
-      </ul>
+      </button>
+      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+        <ul
+          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+        >
+          <li>
+            <nuxt-link
+              to="/"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              exact-active-class="md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+              >Home</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link
+              to="/about"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              exact-active-class="md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+              >About</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link
+              to="/Services"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              exact-active-class="md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+              >Services</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link
+              to="/Pricing"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              exact-active-class="md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+              >Pricing</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link
+              to="/Contact"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              exact-active-class="md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+              >Contact</nuxt-link
+            >
+          </li>
+          <li>
+            <button
+              id="theme-toggle"
+              @click="toggleDark()"
+              type="button"
+              class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+            >
+              {{ isDark ? "Dark" : "light" }}
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
-
+  </nav>
 </template>
--- components/Pokemons.vue
+++ components/Pokemons.vue
@@ -1,40 +1,65 @@
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-    <div v-for="pokemon in pokemonList" :key="pokemon.name" :class="pokemonCardClass(pokemon.types)" class="relative rounded-lg overflow-hidden border border-gray-200 hover:shadow-lg transform hover:scale-105 transition duration-300 ease-in-out">
+  <div
+    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"
+  >
+    <div
+      v-for="pokemon in pokemonList"
+      :key="pokemon.name"
+      :class="pokemonCardClass(pokemon.types)"
+      class="relative rounded-lg overflow-hidden border border-gray-200 hover:shadow-lg transform hover:scale-105 transition duration-300 ease-in-out"
+    >
       <div class="p-4">
-        <img :src="pokemon.sprites.front_default" :alt="pokemon.name" class="mx-auto" style="max-width: 120px;">
+        <img
+          :src="pokemon.sprites.front_default"
+          :alt="pokemon.name"
+          class="mx-auto"
+          style="max-width: 120px"
+        />
         <p class="text-center font-bold mt-2">{{ pokemon.name }}</p>
         <div class="grid grid-cols-2 gap-2 mt-4">
-          <div v-for="(stat, index) in pokemon.stats" :key="index" class="text-center">
-            <p class="text-xs text-gray-500">{{ stat.stat.name.replace('-', ' ') }}</p>
+          <div
+            v-for="(stat, index) in pokemon.stats"
+            :key="index"
+            class="text-center"
+          >
+            <p class="text-xs text-gray-500">
+              {{ stat.stat.name.replace("-", " ") }}
+            </p>
             <p class="font-semibold">{{ stat.base_stat }}</p>
           </div>
         </div>
       </div>
-      <div class="absolute inset-0 rounded-lg border-2 border-transparent animate-pulse"></div> <!-- Glowing effect -->
+      <div
+        class="absolute inset-0 rounded-lg border-2 border-transparent animate-pulse"
+      ></div>
+      <!-- Glowing effect -->
       <div class="p-2 text-center relative z-10">
-        <p class="text-xs italic">Type: {{ pokemon.types.map(type => type.type.name).join(', ') }}</p>
+        <p class="text-xs italic">
+          Type: {{ pokemon.types.map((type) => type.type.name).join(", ") }}
+        </p>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from "vue";
 
 const pokemonList = ref([]);
 
 const fetchPokemonData = async () => {
   try {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20');
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
     const data = await response.json();
-    const pokemonDetails = await Promise.all(data.results.map(async (result: any) => {
-      const response = await fetch(result.url);
-      return response.json();
-    }));
+    const pokemonDetails = await Promise.all(
+      data.results.map(async (result: any) => {
+        const response = await fetch(result.url);
+        return response.json();
+      })
+    );
     pokemonList.value = pokemonDetails;
   } catch (error) {
-    console.error('Error fetching Pokémon data:', error);
+    console.error("Error fetching Pokémon data:", error);
   }
 };
 
@@ -46,47 +71,47 @@ const pokemonCardClass = (types: any[]) => {
   if (types.length > 0) {
     const primaryType = types[0].type.name;
     switch (primaryType) {
-      case 'grass':
-        return 'bg-green-400 hover:bg-green-500';
-      case 'fire':
-        return 'bg-red-400 hover:bg-red-500';
-      case 'water':
-        return 'bg-blue-400 hover:bg-blue-500';
-      case 'electric':
-        return 'bg-yellow-400 hover:bg-yellow-500';
-      case 'poison':
-        return 'bg-purple-400 hover:bg-purple-500';
-      case 'bug':
-        return 'bg-green-400 hover:bg-green-500';
-      case 'normal':
-        return 'bg-gray-400 hover:bg-gray-500';
-      case 'flying':
-        return 'bg-blue-400 hover:bg-blue-500';
-      case 'fighting':
-        return 'bg-red-400 hover:bg-red-500';
-      case 'psychic':
-        return 'bg-purple-400 hover:bg-purple-500';
-      case 'ground':
-        return 'bg-yellow-400 hover:bg-yellow-500';
-      case 'rock':
-        return 'bg-gray-400 hover:bg-gray-500';
-      case 'ghost':
-        return 'bg-indigo-400 hover:bg-indigo-500';
-      case 'ice':
-        return 'bg-blue-400 hover:bg-blue-500';
-      case 'dragon':
-        return 'bg-indigo-400 hover:bg-indigo-500';
-      case 'dark':
-        return 'bg-gray-400 hover:bg-gray-500';
-      case 'steel':
-        return 'bg-gray-400 hover:bg-gray-500';
-      case 'fairy':
-        return 'bg-pink-400 hover:bg-pink-500';
+      case "grass":
+        return "bg-green-400 hover:bg-green-500";
+      case "fire":
+        return "bg-red-400 hover:bg-red-500";
+      case "water":
+        return "bg-blue-400 hover:bg-blue-500";
+      case "electric":
+        return "bg-yellow-400 hover:bg-yellow-500";
+      case "poison":
+        return "bg-purple-400 hover:bg-purple-500";
+      case "bug":
+        return "bg-green-400 hover:bg-green-500";
+      case "normal":
+        return "bg-gray-400 hover:bg-gray-500";
+      case "flying":
+        return "bg-blue-400 hover:bg-blue-500";
+      case "fighting":
+        return "bg-red-400 hover:bg-red-500";
+      case "psychic":
+        return "bg-purple-400 hover:bg-purple-500";
+      case "ground":
+        return "bg-yellow-400 hover:bg-yellow-500";
+      case "rock":
+        return "bg-gray-400 hover:bg-gray-500";
+      case "ghost":
+        return "bg-indigo-400 hover:bg-indigo-500";
+      case "ice":
+        return "bg-blue-400 hover:bg-blue-500";
+      case "dragon":
+        return "bg-indigo-400 hover:bg-indigo-500";
+      case "dark":
+        return "bg-gray-400 hover:bg-gray-500";
+      case "steel":
+        return "bg-gray-400 hover:bg-gray-500";
+      case "fairy":
+        return "bg-pink-400 hover:bg-pink-500";
       default:
-        return 'bg-gray-400 hover:bg-gray-500';
+        return "bg-gray-400 hover:bg-gray-500";
     }
   } else {
-    return 'bg-gray-400 hover:bg-gray-500';
+    return "bg-gray-400 hover:bg-gray-500";
   }
 };
 </script>
@@ -208,7 +233,8 @@ const pokemonCardClass = (types: any[]) => {
 
 /* Glowing effect animation */
 @keyframes pulse {
-  0%, 100% {
+  0%,
+  100% {
     border-color: rgba(255, 255, 255, 0.8); /* White */
     box-shadow: 0 0 0 0px rgba(255, 255, 255, 0.8); /* White glow */
   }
